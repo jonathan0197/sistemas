0b996fa9e385ec4a2a9bb1292c3641cf0540310c
jonathan0197 <75732883+jonathan0197@users.noreply.github.com>
2020-12-09 11:00:27 -0600

    modificacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,9 @@
                         </g>
                     </svg>
                 </div>
+                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <h1>Ing en sistemas computacionales Jonathan alvarez leyva </h1>
+                </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
